699af0c1dc63a760dcd50b0d5883533fdb6df855
Muhammad Nasrullah <nasrullahcoder@gmail.com>
2025-08-27 20:19:36 +0500

    Adding Files
    Adding files to projects.

---
+++ lib/services/location.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  late double latitude;
+  late double longitude;
+
+  final LocationSettings locationSettings = LocationSettings(
+    accuracy: LocationAccuracy.low,
+    distanceFilter: 100,
+  );
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          locationSettings: locationSettings);
+
+    latitude = position.latitude;
+    longitude = position.longitude;
+    }
+    catch(e){
+      print(e);
+    }
+  }
+}
